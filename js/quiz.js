8affc6d65b0037884908c0344fb3fdacadc562ff
Faerovia <94494761+ErinnMatesi@users.noreply.github.com>
2022-01-16 15:33:20 -0800

    changed a few files, committing to save progress. queryselector variables made in js, basic CSS put in place.

--- js/quiz.js
+++ js/quiz.js
@@ -7,4 +7,24 @@
  */
 console.log(questions);
 
+// will need to become hidden when highscores are being shown
+var heading = document.querySelector("heading");
+// needs to change was shows in quiz section to the high scores
+var viewHighscores = document.querySelector("viewHighscores");
+// will need to display the setInterval timer
+var timerLocation = document.querySelector("timerLocation");
+// what is shown on page load
+var welcome = document.querySelector("welcome");
+// will hold the quiz
+var quiz = document.querySelector("quiz");
+var question = document.querySelector("question");
+var button1 = document.querySelector("button1");
+var button2 = document.querySelector("button2");
+var button3 = document.querySelector("button3");
+var button4 = document.querySelector("button4");
+// will need to be hidden until an answer is chosen
+var answer = document.querySelector("answer");
+// shows highscores, will be hidden until viewHighscores button is pushed
+var highScores = document.querySelector("highScores");
+
 
